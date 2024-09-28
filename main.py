f127107aaefc5792945fb6f454d73af6fc7926aa
Magnetize8953 <aha6@charlotte.edu>
2024-09-28 14:02:54 -0400

    add command arguments to main.py
    args for location, assignment_id, section_num, and time modifier

--- main.py
+++ main.py
@@ -15,22 +15,29 @@ import scanner
 import canvas
 import time
 import os
+import argparse
+
+parser = argparse.ArgumentParser(prog='Scanner Push', description="a quick and dirty solution to automating grabbing CCI Events scanner data")
+parser.add_argument('location', choices=['cone', 'woodward'], help='the building location of the section')
+parser.add_argument('assignment_id', type=int, help='the canvas id for the attendance assignment being graded')
+parser.add_argument('section_num', type=int, help='the number for the course section being checked')
+parser.add_argument('-t', '--time_modifier', default=1, type=float, help='modifier to the amount of time the scanner looks back')
+args = parser.parse_args()
 
 if __name__ == "__main__":
     # woodward is 10, cone is 9
-    space_id = 10
-    scanner.download_scanner_data(space_id, 60)
-
-    # set file name
-    file_name = ""
-    if space_id == 10:
+    if args.location == 'woodward':
+        space_id = 10
         file_name = "Woodward 120 Fall Term 2024_sign_in_export.csv"
-    elif space_id == 9:
+    else:
+        space_id = 9
         file_name = "Cone 175 Fall Term 2024_sign_in_export.csv"
+
+    scanner.download_scanner_data(space_id, 60*args.time_modifier)
 
     # wait for file to download
     while file_name not in os.listdir():
         time.sleep(5)
         print('\twaiting for download...')
 
-    # canvas.push_attendance_to_canvas(-1, -1, file_name)
+    canvas.push_attendance_to_canvas(args.assignment_id, args.section_num, file_name)
